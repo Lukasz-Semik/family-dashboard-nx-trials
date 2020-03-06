cdbb4e3c5956d938e0f5e8749b1294109ada42ac
Lukasz <djpluki@gmail.com>
2020-03-06 13:29:10 +0100

    make small fixes

--- apps/backend/src/app/modules/auth/auth.module.ts
+++ apps/backend/src/app/modules/auth/auth.module.ts
@@ -3,12 +3,12 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
 import { EnvService } from '@app-be/modules-global/env/env.service';
+import { UserSerializatorService } from '@app-be/serializators/user/userSerializator.service';
 
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { UserService } from '../user/services/user.service';
 import { JwtStrategy } from './jwt.strategy';
-import { UserSerializatorService } from '@app-be/serializators/user/userSerializator.service';
 
 @Module({
   imports: [
